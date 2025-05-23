185efa37932abf8cfefdce12c7a68082f30d5fad
Giuliana Vecchi <giulivec@gmail.com>
2025-05-23 13:13:04 +0200

    terzo commit di prova

--- day_01/HelloWorld.java
+++ day_01/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//provo ad aggiungere una riga e rifare il git
+		//terza prova di commit
 		System.out.println("Hello World");
 	}
 

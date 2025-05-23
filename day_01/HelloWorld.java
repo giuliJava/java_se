1b0f0599b522582548f1d4356ba033f30356630e
Giuliana Vecchi <giulivec@gmail.com>
2025-05-23 14:32:58 +0200

    quinta prova

--- day_01/HelloWorld.java
+++ day_01/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//quarta prova
+		//quinta prova
 		System.out.println("Hello World");
 	}
 

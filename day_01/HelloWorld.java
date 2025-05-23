c5af92195979a3e19b14ffc40f3940383e6d39c7
Giuliana Vecchi <giulivec@gmail.com>
2025-05-23 12:55:24 +0200

    ho rifatto il commit

--- day_01/HelloWorld.java
+++ day_01/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//provo ad aggiungere una riga e rifare il git
 		System.out.println("Hello World");
 	}
 
